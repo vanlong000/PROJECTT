b7552f8ed0f47ea1236e3282ec0e7e2a879c65a5
vanlong000 <joy.lee.2606@gmail.com>
2023-08-08 06:58:52 +0700

    add code

--- src/pages/Product_details1.js
+++ src/pages/Product_details1.js
@@ -9,7 +9,8 @@ function Product_details1(){
          <img src="images/breakfast/brett-jordan-8xt8-HIFqc8-unsplash.jpg" class="img-fluid menu-image" alt=""/>
            <div class="menu-info d-flex flex-wrap align-items-center">
                <h4 class="mb-0">Fresh Start</h4>
-                <span class="price-tag bg-white shadow-lg ms-4"><small>$</small>24.50</span>
+                <span class="price-tag bg-white shadow-lg ms-4"><small>vnd
+                  </small>24.50</span>
                    <div class="d-flex flex-wrap align-items-center w-100 mt-2">
                     <h6 class="reviews-text mb-0 me-3">4.4/5</h6>
                     <div class="reviews-stars">
@@ -19,7 +20,8 @@ function Product_details1(){
                        <i class="bi-star-fill reviews-icon"></i>
                        <i class="bi-star reviews-icon"></i>
                     </div>
-                    <p class="reviews-text mb-0 ms-4">128 Reviews</p>
+                    <p class="reviews-text mb-0 ms-4">129
+                     Reviews</p>
                    </div>
            </div>
          </div>
